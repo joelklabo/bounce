29542bf0f72e654ccd7f8bbfcff89ba33ab52ca3
Joel Klabo <joelklabo@gmail.com>
2012-11-01 23:38:44 -0700

    better collision detection

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var canvas = document.getElementById("canvas"),
-		ctx    = canvas.getContext("2d");
+		ctx    = canvas.getContext("2d")
 
 var Box = BoxConstructor()
 
-var boxNum = 20,
+var boxNum = 3,
     W = 350,
 		H = 450;
 
@@ -12,7 +12,7 @@ canvas.width  = W;
 
 var boxes = [], 
 		gravity = 0.2,
-		bounceFactor = 1;
+		bounceFactor = 1.0;
 
 function BoxConstructor () {
 
@@ -21,8 +21,8 @@ function BoxConstructor () {
   return function (opts) {
     this.id  = id++
 
-    this.height = opts.height || 15
-    this.width  = opts.width  || 15
+    this.height = opts.height || 30 
+    this.width  = opts.width  || 30 
 
     this.color = opts.color || "red"
 
@@ -36,34 +36,55 @@ function BoxConstructor () {
 	  	ctx.beginPath();
 	  	ctx.fillStyle = self.color;
 	  	ctx.fillRect(self.x, self.y, self.height, self.width);
+	  	ctx.fillStyle = "white";
+      ctx.font = "15pt arial";
+      ctx.fillText(self.id, (self.x + (self.width / 2 - 7.5)), (self.y + (self.height/2 + 7.5)));
 	  	ctx.closePath();
     }
   }
 
 }
 
-for (var i = 0; i < boxNum; i++) {
-  var opts = {}, 
-      rand = Math.random(),
-      sign
+var collisions = true
+var collision = false
+var count = 0
 
-  function sign () {
-    if (Math.random() < 0.5) {
-      return -1
-    } else {
-      return 1
+while (collisions) {
+  for (var i = 0; i < boxNum; i++) {
+    var opts = {}, 
+        sign
+  
+    function sign () {
+      if (Math.random() < 0.5) {
+        return -1
+      } else {
+        return 1
+      }
+    }
+  
+    opts.x = Math.floor((W - 20) * Math.random())
+    opts.y = Math.floor((H - 20) * Math.random())
+    
+    opts.vx = Math.floor(5 * Math.random()) * sign()
+    opts.vy = Math.floor(5 * Math.random()) * sign()
+
+    boxes.push(new Box(opts))
+  }
+  collision = false
+  for (var j = 0; j < boxes.length; j++) {
+    for (var k = 0; k <boxes.length; k++) {
+      if (boxes[j].id != boxes[k].id) {
+        // Check for side collisions
+        if (sideCollisionRangeY(boxes[j], boxes[k]) && verticalCollisionRangeX(boxes[j], boxes[k])) {
+          collision = true 
+        }
+      }
     }
   }
-
-  opts.x = Math.floor(W * rand)
-  opts.y = Math.floor(H * rand)
-  
-  opts.vx = Math.floor(20 * rand) * sign()
-  opts.vy = Math.floor(20 * rand) * sign()
-
-  boxes.push(new Box(opts))
+  if (!collision) { collisions = false }
 }
 
+console.log(count)
 
 function clearCanvas() {
 	ctx.clearRect(0, 0, W, H);
@@ -76,32 +97,62 @@ function updateAll () {
   }
 }
 
+function sideCollisionRangeY (box, otherBox) {
+  var otherBoxTopRange = otherBox.y - box.height
+  var otherBoxBottomRange = otherBox.y + otherBox.height 
+  
+  if (box.y >= otherBoxTopRange && box.y <= otherBoxBottomRange) {
+    return true
+  }
+  return false
+}
+
+function sideCollisionX (box, otherBox) {
+  var otherBoxLeft = otherBox.x - box.width
+  var otherBoxRight = otherBox.x + otherBox.width 
+
+  if (box.x == otherBoxLeft || box.x == otherBoxRight) {
+    return true
+  }
+  return false
+}
+
+function verticalCollisionRangeX (box, otherBox) {
+  var otherBoxLeftRange = otherBox.x - box.width
+  var otherBoxRightRange = otherBox.x + otherBox.width 
+  
+  if (box.x >= otherBoxLeftRange && box.x <= otherBoxRightRange) {
+    return true
+  }
+  return false
+}
+
+function verticalCollisionY (box, otherBox) {
+  var otherBoxTop = otherBox.y - box.height
+  var otherBoxBottom = otherBox.y + otherBox.height 
+
+  if (box.y == otherBoxTop || box.y == otherBoxBottom) {
+    return true
+  }
+  return false
+}
+
 function checkCollisions (box, otherBox) {
   if (box.id != otherBox.id) {
     // Check for side collisions
-    if ((box.x <= otherBox.x + otherBox.width && box.x + box.width >= otherBox.x) && 
-        (box.y + box.height >= otherBox.y && box.y <= otherBox.y + otherBox.height)) {
-      box.vy *= -bounceFactor
-    }
-    if ((box.x + box.width >= otherBox.x && box.x + box.width <= otherBox.x + otherBox.width) && 
-        (box.y + box.height >= otherBox.y && box.y <= otherBox.y + otherBox.height)) {
-      box.vy *= -bounceFactor
+    if (sideCollisionRangeY(box, otherBox) && sideCollisionX(box, otherBox)) {
+      box.vx *= -bounceFactor
+      otherBox.vx *= -bounceFactor
     }
     // Check for top/bottom collisions
-    if ((box.y <= otherBox.y + otherBox.height && box.y + box.height >= otherBox.y) && 
-        (box.x + box.width >= otherBox.x && box.x <= otherBox.x + otherBox.width)) {
-      box.vx *= -bounceFactor
-    }
-    if ((box.y + box.height >= otherBox.y && box.y + box.height <= otherBox.y + otherBox.height) && 
-        (box.x + box.width >= otherBox.x && box.x <= otherBox.x + otherBox.width)) {
-      box.vx *= -bounceFactor
+    if (verticalCollisionRangeX(box, otherBox) && verticalCollisionY(box, otherBox)) {
+      box.vy *= -bounceFactor
+      otherBox.vy *= -bounceFactor
     }
   }
 }
 
 function update (box) {
-
-	box.draw();
   
   // Wall collision
 	box.y += box.vy;
@@ -109,8 +160,7 @@ function update (box) {
 		box.y = H - box.height;
 		box.vy *= -bounceFactor;
 	}
-  if(box.y - box.height <= 0) {
-		box.y = 0 + box.height;
+  if(box.y <= 0) {
 		box.vy *= -bounceFactor;
 	}
 	box.x += box.vx;
@@ -118,8 +168,7 @@ function update (box) {
 		box.x = W - box.width;
 		box.vx *= -bounceFactor;
 	}
-  if(box.x - box.width <= 0) {
-		box.x = 0 + box.width;
+  if(box.x <= 0) {
 		box.vx *= -bounceFactor;
 	}
 
@@ -127,6 +176,8 @@ function update (box) {
   for (var k = 0; k < boxes.length; k++) {
     checkCollisions(box, boxes[k])
   } 
+
+	box.draw();
 
 }
 
